3f2975bc25998a8317b97f1fa188c05a9840d45f
Andy Bryant <ar.bryant@btinternet.com>
2018-01-05 10:28:08 +0000

    add form syncing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { Observable } from 'rxjs/Observable';
+import { select, dispatch } from '@angular-redux/store';
+import { FormBuilder, Validators } from '@angular/forms';
+import { OnInit, OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
+import { NgRedux } from '@angular-redux/store';
+import * as R from 'ramda';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/debounce';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/merge';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  myForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    age: ['', Validators.required]
+  });
+  @select() myForm$: Observable<{ name: string }>;
+  @select() isOn$: Observable<boolean>;
+  @dispatch() formChanged = value => ({ type: 'MY_FORM_CHANGE', value });
+  @dispatch() turnOn = () => ({ type: 'TURN_ON' });
+  @dispatch() turnOff = () => ({ type: 'TURN_OFF' });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    const form$ = this.myForm.valueChanges.map(x => ({ val: x, isForm: true }));
+    const redux$ = this.myForm$.map(x => ({ val: x, isForm: false }));
+    form$
+      .debounce(() => Observable.timer(300))
+      .merge(redux$)
+      .distinctUntilChanged((x, y) => R.equals(x.val, y.val))
+      .forEach(
+        ({ val, isForm }) =>
+          isForm ? this.formChanged(val) : this.myForm.setValue(val)
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,71 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  NgReduxModule,
+  NgRedux,
+  DevToolsExtension
+} from '@angular-redux/store';
+import { AnyAction, Reducer, combineReducers } from 'redux';
 import { AppComponent } from './app.component';
+import * as R from 'ramda';
 
+interface IAppState {
+  isOn: boolean;
+  myForm: {
+    name: string;
+    age: string;
+  };
+}
+
+const isOn: Reducer<boolean> = (state = false, action) => {
+  switch (action.type) {
+    case 'TURN_ON':
+      return true;
+    case 'TURN_OFF':
+      return false;
+    default:
+      return state;
+  }
+};
+
+const myForm: Reducer<any> = (state = { name: '', age: '' }, action) => {
+  switch (action.type) {
+    case 'MY_FORM_CHANGE':
+      return R.equals(state, action.value) ? state : action.value;
+    default:
+      return state;
+  }
+};
+
+const reducer: Reducer<IAppState> = combineReducers({ isOn, myForm });
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgReduxModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    private ngRedux: NgRedux<IAppState>,
+    private devTools: DevToolsExtension
+  ) {
+    ngRedux.configureStore(
+      reducer,
+      {
+        isOn: false,
+        myForm: {
+          name: '',
+          age: ''
+        }
+      },
+      [],
+      devTools.isEnabled() ? [devTools.enhancer()] : []
+    );
+  }
+}
